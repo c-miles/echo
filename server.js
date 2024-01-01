414ae28cb74761916d2a21c6719413e2950aedf7
Chris Miles <cmiles@obm.com>
2024-01-01 15:46:52 -0500

    install express, ignore node modules

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const cors = require('cors');
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  cors()(req, res, () => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'feedback loop created!' }));
-  });
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express!" });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
