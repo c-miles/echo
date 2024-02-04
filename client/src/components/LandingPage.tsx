85f638882f692d585e80704f9f8131082942e6f5
Chris Miles <cmiles@obm.com>
2024-02-04 16:08:36 -0500

    navigate returning authenticated users from landing page to dashboard

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,11 +1,20 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useEffect } from "react";
 
+import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+
 import AuthenticationButton from "./AuthenticationButton";
 
 const LandingPage: React.FC = () => {
   const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
   const styles = useStyles();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <div style={styles.container}>
